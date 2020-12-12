2edfd31a4faa9063b6339cec3f023fd35dfd2d60
marilliaalves <mmarillialves@gmail.com>
2020-12-11 23:25:54 -0300

    rotas

--- src/routes/certifiedRoute.js
+++ src/routes/certifiedRoute.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const controller = require("../controllers/certifiedController")
 
 
-router.get("/", controller.getAllCertifies)
-router.get("/:id", controller.getById)
+router.get("/certifies", controller.getAllCertifies)
+router.get("/certifies/:id", controller.getById)
 router.get("/year/:year", controller.getYear)
 router.post("/", controller.postCertifies) // inclui novos
 router.put("/:id", controller.putCertifies) // altera dados do registro
